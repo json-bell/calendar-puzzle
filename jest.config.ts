857ab3cace1243ab6a68a039bdbb920e27e35451
Jason Bell <jasonbell143@gmail.com>
2025-02-05 21:57:23 +0000

    adds jest

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts", "!**/vendor/**"],
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
+  transform: {
+    ".(ts|tsx)": "ts-jest",
+  },
+
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/coverage",
+    "package.json",
+    "package-lock.json",
+    "reportWebVitals.ts",
+    "setupTests.ts",
+    "index.tsx",
+  ],
+};
+
+export default config;
