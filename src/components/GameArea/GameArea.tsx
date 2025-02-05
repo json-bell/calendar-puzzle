6fa6d7077837ad5520af3d675d65ffb6343edab2
Jason Bell <jasonbell143@gmail.com>
2025-02-05 22:01:48 +0000

    adds visual clue to successful edit

---
+++ src/components/GameArea/GameArea.tsx
@@ -0,0 +1,11 @@
+import styles from "./GameArea.module.css";
+
+const GameArea = () => {
+  return (
+    <p className={styles.testStyle}>
+      Hello! Just making sure I can actually see the things I modify
+    </p>
+  );
+};
+
+export default GameArea;
