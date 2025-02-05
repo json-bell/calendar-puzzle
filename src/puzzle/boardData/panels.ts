d9a8f72deb8143697367561b26b32d22854bb8bc
Jason Bell <jasonbell143@gmail.com>
2025-02-05 23:38:42 +0000

    writes rawSetupData

--- src/puzzle/boardData/panels.ts
+++ src/puzzle/boardData/panels.ts
@@ -33,4 +33,11 @@ export const months = [
 ] as const;
 export type Months = (typeof months)[number];
 
-const rawSetupData = [];
+const rawSetupData = [
+  ["jan", "feb", "mar", "apr", "1", "2", "3", "mon", "tue"],
+  ["may", "4", "5", "6", "7", "8", "9", "wed", "empty"],
+  ["jun", "10", "11", "12", "13", "31", "15", "thu", "empty"],
+  ["jul", "16", "17", "18", "19", "20", "21", "fri", "sat"],
+  ["aug", "22", "23", "24", "25", "26", "27", "empty", "sun"],
+  ["sep", "oct", "nov", "dec", "28", "29", "30", "14", "blocked"],
+];
