4715061ac86b88e99a95d9b54cae9134abbce320
Jason Bell <jasonbell143@gmail.com>
2025-02-06 04:31:07 +0000

    adds puzzle pieces

--- src/components/Game/PanelBoard/PanelBoard.tsx
+++ src/components/Game/PanelBoard/PanelBoard.tsx
@@ -2,18 +2,27 @@ import React from "react";
 import styles from "./PanelBoard.module.css";
 import boardPanels from "../../../puzzle/boardPanels";
 import BoardPanel from "../PanelTile/BoardPanel";
+import puzzlePieces from "../../../puzzle/puzzlePieces";
+import PuzzlePiece from "../PuzzlePiece/PuzzlePiece";
 
 const PanelBoard: React.FC = () => {
   return (
-    <div className={styles.boardWrapper}>
-      <div className={styles.boardGrid}>
-        {boardPanels.map((panelRow) =>
-          panelRow.map((panel) => {
-            return <BoardPanel panel={panel} />;
-          })
-        )}
+    <>
+      <div className={styles.boardWrapper}>
+        <div className={styles.boardGrid}>
+          {boardPanels.map((panelRow) =>
+            panelRow.map((panel) => {
+              return <BoardPanel panel={panel} />;
+            })
+          )}
+        </div>
       </div>
-    </div>
+      <div style={{ display: "flex", gap: "30px", flexWrap: "wrap" }}>
+        {puzzlePieces.map((piece) => (
+          <PuzzlePiece piece={piece} />
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/components/Game/PuzzlePiece/PuzzlePiece.tsx
+++ src/components/Game/PuzzlePiece/PuzzlePiece.tsx
@@ -1,12 +1,29 @@
 import React from "react";
-// import styles from "./PuzzlePiece.module.css";
+import { PuzzlePieceType } from "../../../puzzle/puzzlePieces/getPuzzlePieces";
+import styles from "./PuzzlePiece.module.css";
+import joinStyles from "../../../utils/concatStyles/concatStyles";
 
 export interface PuzzlePieceProps {
-  _: never;
+  piece: PuzzlePieceType;
 }
 
-const PuzzlePiece: React.FC<PuzzlePieceProps> = () => {
-  return <></>;
+const PuzzlePiece: React.FC<PuzzlePieceProps> = ({ piece }) => {
+  return (
+    <div className={styles.puzzlePiece}>
+      {piece.shape.map((row) => (
+        <div className={styles.pieceRow}>
+          {row.map((isSquare) => (
+            <div
+              className={joinStyles(
+                styles.square,
+                isSquare && styles.presentSquare
+              )}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PuzzlePiece;
