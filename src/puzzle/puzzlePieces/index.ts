2d10f7f184e5dd9c6e6840f5639133116b1b1139
Jason Bell <jasonbell143@gmail.com>
2025-02-06 04:20:11 +0000

    generates puzzlePieces

--- src/puzzle/puzzlePieces/index.ts
+++ src/puzzle/puzzlePieces/index.ts
@@ -1,5 +1,91 @@
-// import { PuzzlePieceType } from "./generatePuzzlePieces";
+// This is a script generated file - do not edit. Run `yarn generate-panels` to re-generate the file.
 
-// const puzzlePieces: PuzzlePieceType = [];
+import { PuzzlePieceType } from "./getPuzzlePieces";
 
-// export default puzzlePieces;
+const puzzlePieces: PuzzlePieceType[] = [
+  {
+    "shape": [
+      [ true, true, true ],
+      [ true, false, false ],
+      [ true, false, false ]
+    ],
+    "pieceId": "111100100"
+  },
+  {
+    "shape": [
+      [ true, true, true ],
+      [ false, true, false ],
+      [ false, true, false ]
+    ],
+    "pieceId": "111010010"
+  },
+  {
+    "shape": [
+      [ true, true, false ],
+      [ true, false, false ],
+      [ true, false, false ],
+      [ true, false, false ]
+    ],
+    "pieceId": "110100100100"
+  },
+  {
+    "shape": [
+      [ true, false, false ],
+      [ true, false, false ],
+      [ true, false, false ],
+      [ true, false, false ],
+      [ true, false, false ]
+    ],
+    "pieceId": "100100100100100"
+  },
+  {
+    "shape": [
+      [ false, false, true ],
+      [ true, true, true ],
+      [ true, false, false ]
+    ],
+    "pieceId": "001111100"
+  },
+  {
+    "shape": [
+      [ true, false, false ],
+      [ true, true, false ],
+      [ false, true, false ],
+      [ false, true, false ]
+    ],
+    "pieceId": "100110010010"
+  },
+  {
+    "shape": [
+      [ true, false, false ],
+      [ true, true, true ],
+      [ false, true, false ]
+    ],
+    "pieceId": "100111010"
+  },
+  {
+    "shape": [
+      [ true, false, false ],
+      [ true, true, false ],
+      [ true, false, false ],
+      [ true, false, false ]
+    ],
+    "pieceId": "100110100100"
+  },
+  {
+    "shape": [
+      [ true, true, true ],
+      [ true, true, false ]
+    ],
+    "pieceId": "111110"
+  },
+  {
+    "shape": [
+      [ true, true, true ],
+      [ true, false, true ]
+    ],
+    "pieceId": "111101"
+  }
+];
+  
+  export default puzzlePieces;
