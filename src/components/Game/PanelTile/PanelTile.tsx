96b393b0152ef2185b53f1a075d761b8bb32fc76
Jason Bell <jasonbell143@gmail.com>
2025-02-06 03:32:23 +0000

    styles empty and wall panels

--- src/components/Game/PanelTile/PanelTile.tsx
+++ src/components/Game/PanelTile/PanelTile.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import styles from "./Panel.module.css";
+import styles from "./PanelTile.module.css";
 import { Panel } from "../../../puzzle/boardData/panels";
+import concatStyles from "../../../utils/concatStyles/concatStyles";
 
 export interface PanelProps {
   panel: Panel;
 }
 
 const PanelTile: React.FC<PanelProps> = ({ panel }) => {
-  return <div className={styles.panel}>{panel.content}</div>;
+  if (panel.type === "wall")
+    return <div className={concatStyles(styles.panel, styles.wall)} />;
+  return (
+    <div
+      className={concatStyles(
+        styles.panel,
+        panel.type === "dayNumber" && styles.numberPanel
+      )}
+    >
+      {panel.type !== "empty" && panel.content}
+    </div>
+  );
 };
 
 export default PanelTile;
