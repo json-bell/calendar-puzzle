93db2f382458a6a3b487ba30b1f05c7dc4ddbd7e
Jason Bell <jason.bell@magairports.com>
2025-02-24 23:17:01 +0000

    integrates placed pieces within BoardPanel

--- src/components/Game/PanelTile/BoardPanel.tsx
+++ src/components/Game/PanelTile/BoardPanel.tsx
@@ -4,22 +4,28 @@ import { Panel } from "../../../puzzle/panelTypes";
 import { formatContent } from "../../../puzzle/boardPanels/utils/formatContent";
 import useGameState from "../../../context/Game/state";
 import { useState } from "react";
-import PuzzlePiece from "../PuzzlePiece/PuzzlePiece";
 import { panelSizeGlobal } from "../../../global/globalVariables";
 import useGameDispatch from "../../../context/Game/dispatch";
 import { Actions } from "../../../context/Game/types";
 import PiecePreview from "../PuzzlePiece/PiecePreview";
+import PlacedCell from "../Cell/PlacedCell";
 
 export interface PanelProps {
   panel: Panel;
 }
 
 const BoardPanel: React.FC<PanelProps> = ({ panel }) => {
-  const { userSelection, board, gamePieces } = useGameState();
+  const { userSelection, board } = useGameState();
   const { selectedCell, selectedPiece } = userSelection;
   const [isPreviewing, setIsPreviewing] = useState(false);
 
   const dispatch = useGameDispatch();
+
+  if (panel.type === "wall")
+    return <div className={cx(styles.panel, styles.wall)} />;
+
+  const coveringCell = board[panel.panelY][panel.panelX].coveredBy;
+
   const handlePlacePiece = () => {
     // if (coveringCell) {
     //   if (coveringCell === selectedPiece) rotatePieceAroundCell();
@@ -36,11 +42,6 @@ const BoardPanel: React.FC<PanelProps> = ({ panel }) => {
     }
   };
 
-  if (panel.type === "wall")
-    return <div className={cx(styles.panel, styles.wall)} />;
-
-  const coveringCell = board[panel.panelY][panel.panelX].coveredBy;
-
   const handlers = {
     onMouseEnter: () => setIsPreviewing(true),
     onMouseLeave: () => setIsPreviewing(false),
@@ -55,12 +56,13 @@ const BoardPanel: React.FC<PanelProps> = ({ panel }) => {
           styles.panel,
           panel.type === "dayNumber" && styles.numberPanel,
           isPreviewing && selectedPiece && styles.previewedPanel,
-          coveringCell && styles.coveredCell
+          coveringCell && styles.coveredPanel
         )}
       >
         {panel.type !== "empty" && formatContent(panel.content)}
       </div>
-      {/* Piece Preview */}
+
+      {/* Selected Piece Preview if Panel is Empty*/}
       {isPreviewing && selectedPiece && (
         <div
           className={cx(styles.piecePreview)}
@@ -75,15 +77,12 @@ const BoardPanel: React.FC<PanelProps> = ({ panel }) => {
 
       {/* ISSUE: CURRENTLY EVERY TIME THERE'S A PIECE IT IS PLACING AN ENTIRE PIECE */}
       {coveringCell && (
-        <div
-          className={cx(styles.piecePlaced)}
-          style={{
-            left: `-${coveringCell.cellX * panelSizeGlobal}px`,
-            top: `-${coveringCell.cellY * panelSizeGlobal}px`,
-          }}
-        >
-          <PuzzlePiece piece={gamePieces[coveringCell.pieceId].piece} />
-        </div>
+        <>
+          <PlacedCell
+            isCellSelected={coveringCell.cellSlug === selectedCell?.cellSlug}
+            isPanelSelected={coveringCell.pieceId === selectedCell?.pieceId}
+          />
+        </>
       )}
     </button>
   );
