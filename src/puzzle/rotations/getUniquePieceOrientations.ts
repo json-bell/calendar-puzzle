a45e022d27c97d99a06d661aaeef4abe14a158ca
Jason Bell <jason.bell@magairports.com>
2025-02-26 02:47:14 +0000

    creates unique piece rotations

---
+++ src/puzzle/rotations/getUniquePieceOrientations.ts
@@ -0,0 +1,40 @@
+import puzzlePieces from "../puzzlePieces";
+import { Piece, PieceShape } from "../pieceTypes";
+import getRotatedPiece from "./getRotatedPiece";
+import { PieceRotation } from "../types";
+import { UniqueOrientation } from "./types";
+
+const getShapeSlug = (rawShape: PieceShape<boolean>): string => {
+  return rawShape
+    .map((row) => row.map((bool) => (bool ? 1 : 0)).join(""))
+    .join("-");
+};
+
+const degrees: PieceRotation[] = [1, 2, 3, 0];
+
+export const getOrientationsFromPiece = (
+  shape: Piece["shape"],
+  pieceId: number
+): UniqueOrientation => {
+  const pieceSlug = getShapeSlug(shape);
+  const rotatedShapes = degrees.map((rotation: PieceRotation): Piece["shape"] =>
+    getRotatedPiece(shape, rotation, 0)
+  );
+  const rotatedSlugs = rotatedShapes.map(getShapeSlug);
+  const flippedShapes = rotatedShapes.map((rotatedShape) =>
+    getRotatedPiece(rotatedShape, 0, 1)
+  );
+  const flippedSlugs = flippedShapes.map(getShapeSlug);
+
+  const rotation = (rotatedSlugs.indexOf(pieceSlug) + 1) as 1 | 2 | 4;
+  const flip = flippedSlugs.includes(pieceSlug) ? 1 : 2;
+  return { pieceId, rotation, flip };
+};
+
+const getUniquePieceOrientations = () => {
+  return puzzlePieces.map(({ shape, pieceId }) =>
+    getOrientationsFromPiece(shape, pieceId)
+  );
+};
+
+export default getUniquePieceOrientations;
---
+++ src/puzzle/rotations/uniqueOrientations.ts
@@ -0,0 +1,18 @@
+// This is a script generated file - do not edit. Run `yarn generate-orientations` to re-generate the file.
+
+import { UniqueOrientation } from "./types";
+
+const uniqueOrientations: UniqueOrientation[] = [
+  { pieceId: 0, rotation: 4, flip: 1 },
+  { pieceId: 1, rotation: 4, flip: 1 },
+  { pieceId: 2, rotation: 4, flip: 2 },
+  { pieceId: 3, rotation: 2, flip: 1 },
+  { pieceId: 4, rotation: 2, flip: 2 },
+  { pieceId: 5, rotation: 4, flip: 2 },
+  { pieceId: 6, rotation: 4, flip: 2 },
+  { pieceId: 7, rotation: 4, flip: 2 },
+  { pieceId: 8, rotation: 4, flip: 2 },
+  { pieceId: 9, rotation: 4, flip: 1 }
+];
+  
+  export default uniqueOrientations;
