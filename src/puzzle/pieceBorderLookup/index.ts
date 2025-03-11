c614c4fddd143185d4739251147c1440969f7005
Jason Bell <jason.bell@magairports.com>
2025-03-11 02:45:33 +0000

    writes script for generating file

---
+++ src/puzzle/pieceBorderLookup/index.ts
@@ -0,0 +1,171 @@
+// This is a script generated file - do not edit. Run `yarn generate-borders` to re-generate the file.
+
+import { PiecesBorderLookup } from "./types";
+
+const pieceBorderLookup: PiecesBorderLookup = [
+  [
+    [
+      { top:  true, left:  true, right: false, bottom: false, top_left:  true, top_right: false, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right: false, bottom:  true, top_left: false, top_right: false, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right:  true, bottom:  true, top_left: false, top_right:  true, bottom_left: false, bottom_right:  true }
+    ],
+    [
+      { top: false, left:  true, right:  true, bottom: false, top_left: false, top_right: false, bottom_left: false, bottom_right: false },
+      { top: false, left: false, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left: false, bottom_right:  true },
+      { top: false, left:  true, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right:  true }
+    ],
+    [
+      { top: false, left:  true, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right:  true },
+      { top:  true, left: false, right:  true, bottom:  true, top_left: false, top_right:  true, bottom_left: false, bottom_right:  true },
+      { top:  true, left:  true, right:  true, bottom:  true, top_left:  true, top_right:  true, bottom_left:  true, bottom_right:  true }
+    ]
+  ],
+  [
+    [
+      { top:  true, left:  true, right: false, bottom:  true, top_left:  true, top_right: false, bottom_left:  true, bottom_right: false },
+      { top:  true, left: false, right: false, bottom: false, top_left: false, top_right: false, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right:  true, bottom:  true, top_left: false, top_right:  true, bottom_left: false, bottom_right:  true }
+    ],
+    [
+      { top: false, left:  true, right: false, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right: false },
+      { top: false, left:  true, right:  true, bottom: false, top_left: false, top_right: false, bottom_left: false, bottom_right: false },
+      { top: false, left: false, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left: false, bottom_right:  true }
+    ],
+    [
+      { top:  true, left:  true, right: false, bottom:  true, top_left:  true, top_right: false, bottom_left:  true, bottom_right: false },
+      { top: false, left:  true, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right:  true },
+      { top:  true, left: false, right:  true, bottom:  true, top_left: false, top_right:  true, bottom_left: false, bottom_right:  true }
+    ]
+  ],
+  [
+    [
+      { top:  true, left:  true, right: false, bottom: false, top_left:  true, top_right: false, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right:  true, bottom:  true, top_left: false, top_right:  true, bottom_left: false, bottom_right:  true }
+    ],
+    [
+      { top: false, left:  true, right:  true, bottom: false, top_left: false, top_right: false, bottom_left: false, bottom_right: false },
+      { top: false, left: false, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left: false, bottom_right:  true }
+    ],
+    [
+      { top: false, left:  true, right:  true, bottom: false, top_left: false, top_right: false, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right:  true, bottom:  true, top_left: false, top_right:  true, bottom_left: false, bottom_right:  true }
+    ],
+    [
+      { top: false, left:  true, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right:  true },
+      { top:  true, left: false, right:  true, bottom:  true, top_left: false, top_right:  true, bottom_left: false, bottom_right:  true }
+    ]
+  ],
+  [
+    [
+      { top:  true, left:  true, right:  true, bottom: false, top_left:  true, top_right:  true, bottom_left: false, bottom_right: false }
+    ],
+    [
+      { top: false, left:  true, right:  true, bottom: false, top_left: false, top_right: false, bottom_left: false, bottom_right: false }
+    ],
+    [
+      { top: false, left:  true, right:  true, bottom: false, top_left: false, top_right: false, bottom_left: false, bottom_right: false }
+    ],
+    [
+      { top: false, left:  true, right:  true, bottom: false, top_left: false, top_right: false, bottom_left: false, bottom_right: false }
+    ],
+    [
+      { top: false, left:  true, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right:  true }
+    ]
+  ],
+  [
+    [
+      { top:  true, left:  true, right:  true, bottom: false, top_left:  true, top_right:  true, bottom_left: false, bottom_right: false },
+      { top:  true, left:  true, right: false, bottom: false, top_left:  true, top_right: false, bottom_left: false, bottom_right: false },
+      { top:  true, left:  true, right:  true, bottom: false, top_left:  true, top_right:  true, bottom_left: false, bottom_right: false }
+    ],
+    [
+      { top:  true, left:  true, right: false, bottom: false, top_left:  true, top_right: false, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right: false, bottom:  true, top_left: false, top_right: false, bottom_left: false, bottom_right: false },
+      { top: false, left: false, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left: false, bottom_right:  true }
+    ],
+    [
+      { top: false, left:  true, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right:  true },
+      { top: false, left: false, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left: false, bottom_right:  true },
+      { top: false, left:  true, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right:  true }
+    ]
+  ],
+  [
+    [
+      { top:  true, left:  true, right:  true, bottom: false, top_left:  true, top_right:  true, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right:  true, bottom: false, top_left: false, top_right:  true, bottom_left: false, bottom_right: false }
+    ],
+    [
+      { top: false, left:  true, right: false, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right: false },
+      { top:  true, left: false, right:  true, bottom: false, top_left: false, top_right:  true, bottom_left: false, bottom_right: false }
+    ],
+    [
+      { top: false, left:  true, right: false, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right: false },
+      { top: false, left:  true, right:  true, bottom: false, top_left: false, top_right: false, bottom_left: false, bottom_right: false }
+    ],
+    [
+      { top:  true, left:  true, right: false, bottom:  true, top_left:  true, top_right: false, bottom_left:  true, bottom_right: false },
+      { top: false, left:  true, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right:  true }
+    ]
+  ],
+  [
+    [
+      { top:  true, left:  true, right:  true, bottom: false, top_left:  true, top_right:  true, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right:  true, bottom: false, top_left: false, top_right:  true, bottom_left: false, bottom_right: false },
+      { top:  true, left:  true, right:  true, bottom: false, top_left:  true, top_right:  true, bottom_left: false, bottom_right: false }
+    ],
+    [
+      { top: false, left:  true, right: false, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right: false },
+      { top:  true, left: false, right: false, bottom: false, top_left: false, top_right: false, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right:  true, bottom:  true, top_left: false, top_right:  true, bottom_left: false, bottom_right:  true }
+    ],
+    [
+      { top: false, left:  true, right: false, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right: false },
+      { top: false, left:  true, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right:  true },
+      { top: false, left: false, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left: false, bottom_right:  true }
+    ]
+  ],
+  [
+    [
+      { top:  true, left:  true, right:  true, bottom: false, top_left:  true, top_right:  true, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right:  true, bottom: false, top_left: false, top_right:  true, bottom_left: false, bottom_right: false }
+    ],
+    [
+      { top: false, left:  true, right: false, bottom: false, top_left: false, top_right: false, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right:  true, bottom:  true, top_left: false, top_right:  true, bottom_left: false, bottom_right:  true }
+    ],
+    [
+      { top: false, left:  true, right:  true, bottom: false, top_left: false, top_right: false, bottom_left: false, bottom_right: false },
+      { top: false, left: false, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left: false, bottom_right:  true }
+    ],
+    [
+      { top: false, left:  true, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right:  true },
+      { top:  true, left: false, right:  true, bottom:  true, top_left: false, top_right:  true, bottom_left: false, bottom_right:  true }
+    ]
+  ],
+  [
+    [
+      { top:  true, left:  true, right: false, bottom: false, top_left:  true, top_right: false, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right: false, bottom: false, top_left: false, top_right: false, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right:  true, bottom:  true, top_left: false, top_right:  true, bottom_left: false, bottom_right:  true }
+    ],
+    [
+      { top: false, left:  true, right: false, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right: false },
+      { top: false, left: false, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left: false, bottom_right:  true },
+      { top: false, left: false, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left: false, bottom_right:  true }
+    ]
+  ],
+  [
+    [
+      { top:  true, left:  true, right: false, bottom: false, top_left:  true, top_right: false, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right: false, bottom:  true, top_left: false, top_right: false, bottom_left: false, bottom_right: false },
+      { top:  true, left: false, right:  true, bottom: false, top_left: false, top_right:  true, bottom_left: false, bottom_right: false }
+    ],
+    [
+      { top: false, left:  true, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right:  true },
+      { top: false, left: false, right: false, bottom:  true, top_left: false, top_right: false, bottom_left: false, bottom_right: false },
+      { top: false, left:  true, right:  true, bottom:  true, top_left: false, top_right: false, bottom_left:  true, bottom_right:  true }
+    ]
+  ]
+];
+  
+  export default pieceBorderLookup;
