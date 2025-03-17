526c224f02f112dea202ba6a1add09eff5d5efe1
Jason Bell <jason.bell@magairports.com>
2025-03-17 19:51:43 +0000

    creates desktop layout

--- src/components/UI/GameControls/GameControls.tsx
+++ src/components/UI/GameControls/GameControls.tsx
@@ -1,3 +1,4 @@
+import ControlButton from "../Button/ControlButton";
 import DeletePieceButton from "../Button/SpecificButtons/DeletePieceButton";
 import styles from "./GameControls.module.css";
 
@@ -5,7 +6,11 @@ const GameControls: React.FC = () => {
   return (
     <div className={styles.gameControlsContainer}>
       <h2 style={{ margin: 0 }}> Menu</h2>
-      <DeletePieceButton />
+      <div className={styles.buttonList}>
+        <DeletePieceButton />
+        <ControlButton text="Choose day" onClick={undefined} inactive />
+        <ControlButton text="Hint" onClick={undefined} inactive />
+      </div>
     </div>
   );
 };
