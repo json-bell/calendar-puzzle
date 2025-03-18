78dff46635be19b45f5a1063840a7b4d2d0784ad
Jason Bell <jason.bell@magairports.com>
2025-03-18 00:00:28 +0000

    implements viewport provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import CSSVarProvider from "./global/CSSVarProvider.tsx";
+import { ViewportProvider } from "./utils/useWindowSize/windowSizeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CSSVarProvider>
-      <App />
-    </CSSVarProvider>
+    <ViewportProvider>
+      <CSSVarProvider>
+        <App />
+      </CSSVarProvider>
+    </ViewportProvider>
   </StrictMode>
 );
