3706c3ceff2815a47ed515c8fa97c6829f32eb2b
Jason Bell <jason.bell@magairports.com>
2025-04-27 21:50:20 +0100

    re-implements increasing rotations

--- src/context/Game/utils/increaseOrientation.ts
+++ src/context/Game/utils/increaseOrientation.ts
@@ -1,41 +1,34 @@
+import { UniqueOrientation } from "../../../puzzle/rotations/types";
 import uniqueOrientations from "../../../puzzle/rotations/uniqueOrientations";
-import { PieceFlipped, PieceRotation } from "../../../puzzle/types";
+import { PieceOrientation, PieceRotation } from "../../../puzzle/types";
 
-const increaseOrientation = (
-  input: { rotation: PieceRotation; flipped: PieceFlipped },
-  pieceId: number
-) => {
-  const { uniqueFlips, uniqueRotations } = uniqueOrientations[pieceId];
-  const rotation = ((input.rotation + 1) % uniqueRotations) as PieceRotation;
-  const didRotationRollOver = rotation === 0;
-  const flipped =
-    uniqueFlips === 1
-      ? 0
-      : ((didRotationRollOver
-          ? (input.flipped + 1) % 2
-          : input.flipped) as PieceFlipped);
-  return { rotation, flipped };
+const rollRotation = ({
+  rotation,
+  uniqueRotations,
+}: {
+  rotation: PieceRotation;
+  uniqueRotations: UniqueOrientation["uniqueRotations"];
+}): PieceRotation => {
+  return ((rotation + 1) % uniqueRotations) as PieceRotation;
 };
 
-// const increaseOrientation = (
-//   input: { rotation: PieceRotation; flipped: PieceFlipped },
-//   pieceId: number
-// ): { rotation: PieceRotation; flipped: PieceFlipped } => {
-//   const { uniqueFlips, uniqueRotations } = uniqueOrientations[pieceId];
+const increaseOrientation = (
+  { rotation, flipped }: PieceOrientation,
+  pieceId: number
+): PieceOrientation => {
+  const { uniqueFlips, uniqueRotations } = uniqueOrientations[pieceId];
 
-//   if (uniqueFlips === 2) {
-//     if (input.flipped === 0 && input.rotation === uniqueRotations - 1)
-//       return { rotation: 1, flipped: 1 };
-//     if (input.flipped === 1 && input.rotation === 0)
-//       return {
-//         rotation: 0,
-//         flipped: 0,
-//       };
-//   }
+  if (uniqueFlips === 2) {
+    if (flipped === 0) {
+      // end of 1st rotation cycle
+      if (rotation + 1 === uniqueRotations) return { rotation: 1, flipped: 1 };
+    } else {
+      // end of mirrored rotation cycle
+      if (rotation === 0) return { rotation: 0, flipped: 0 };
+    }
+  }
 
-//   const rotation = ((input.rotation + 1) % uniqueRotations) as PieceRotation;
-
-//   return { ...input, rotation };
-// };
+  return { flipped, rotation: rollRotation({ rotation, uniqueRotations }) };
+};
 
 export default increaseOrientation;
