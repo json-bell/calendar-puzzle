8039bd4973f10fce505a45bfa58b7bf806c33a46
Jason Bell <jason.bell@magairports.com>
2025-05-21 19:37:37 +0100

    prepares types for solution

--- src/context/ChosenDate/types.ts
+++ src/context/ChosenDate/types.ts
@@ -1,10 +1,13 @@
 import { DayName, DayNumber, Month } from "../../puzzle/boardPanels/dateData";
 import { PanelContent } from "../../puzzle/panelTypes";
 
-export type ChallengeDate = {
-  date: Date;
+export type ChallengeGoal = {
   dayName: DayName;
   dayNumber: DayNumber;
   month: Month;
+};
+
+export type ChallengeDate = ChallengeGoal & {
+  date: Date;
   checkIsChallengeValue: (value: PanelContent) => boolean;
 };
