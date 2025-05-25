db0f28c9a51751700eebd85d4cf2198d3985d237
Jason Bell <jason.bell@magairports.com>
2025-05-25 16:49:54 +0100

    cleans up log Board

--- src/puzzle/utils/logBoard.ts
+++ src/puzzle/utils/logBoard.ts
@@ -1,15 +1,20 @@
+import { CellType } from "../pieceTypes";
 import { Board } from "../types";
 import mapBoard from "./mapBoard";
+
+const joinCoveringCells = (coveringCells: CellType[]) =>
+  coveringCells.map(({ pieceId }) => pieceId).join("");
 
 const logBoard = (board: Board) => {
   const visualBoard = mapBoard(
     board,
     ({ coveringCells }, colIndex, rowIndex) => {
       if (rowIndex === 5 && colIndex === 8) {
-        return `${coveringCells.map(({ pieceId }) => pieceId).join("")}W | `;
+        return `${joinCoveringCells(coveringCells)}W | `;
       }
+
       if (coveringCells.length === 0) return "- | ";
-      return `${coveringCells.map(({ pieceId }) => pieceId).join("")} | `;
+      return `${joinCoveringCells(coveringCells)} | `;
     }
   );
   console.log(visualBoard.map((arr) => arr.join("")).join("\n"));
